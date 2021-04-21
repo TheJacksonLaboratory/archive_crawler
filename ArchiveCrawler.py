0fb05f1be25f5d1566d894d28dfe00b7e944c0e0
kindln <kindln@jax.org>
2021-04-21 18:13:36 -0400

    Added logging

--- ArchiveCrawler.py
+++ ArchiveCrawler.py
@@ -1,8 +1,11 @@
 import argparse
+import datetime
 import json
 import os
 from pathlib import Path
+import logging
 import socket
+import sys
 
 from meta_mapper import MetaMapper
 from metadata_mongo_ingester import MetadataMongoIngester
@@ -39,7 +42,9 @@ class ArchiveCrawler:
         # Get an instance of the KompOmeroSplitter
         self.omero_splitter = KompOmeroSplitter(self.meta_mapper, self.ingester)
 
-       
+        # Initialize the logger
+        self.setup_logger()
+
 
     def crawl_archive(self):
 
@@ -57,8 +62,10 @@ class ArchiveCrawler:
 
         for dir in self.get_json_dirs(self.root_dir):
 
+            logging.info(f"Scanning dir {dir}")
             # Handle the omero data separately
             if self.omero_splitter.is_komp_omero_dir(dir):
+                logging.info("Splitting Omero doc")
                 self.omero_splitter.split_doc(dir)
                 continue
 
@@ -66,6 +73,7 @@ class ArchiveCrawler:
             # metadata found in this directory. If no document is made, skip this directory.
             new_doc = self.meta_mapper.create_new_document(dir)
             if not new_doc:
+                logging.error(f"Could not create new document from directory {dir}")
                 continue
 
             # Adjust the archive path if we're on a BH server
@@ -73,7 +81,12 @@ class ArchiveCrawler:
                 new_doc["archived_path"] = new_doc["archived_path"].replace('/archive', '/bharchive')
 
             # Ingest the document into mongo the collection
-            self.ingester.ingest_document(new_doc)
+            msg = self.ingester.ingest_document(new_doc)
+            if msg:
+                # The ingester only returns a message if there was an error.
+                logging.error(f"Could not ingest document to mongo for directory {dir}")
+            else:
+                logging.info(f"Successfully ingested document for directory {dir}")
 
 
     def get_json_dirs(self, root_dir):
@@ -91,12 +104,44 @@ class ArchiveCrawler:
         """
 
         json_dirs = set()
+        logging.debug("Seeking archive directories with JSON files...")
         json_filenames = Path(root_dir).rglob("*.json")
         for filename in json_filenames:
             dir = os.path.dirname(filename)
+            logging.debug(f"Found directory {dir}")
             json_dirs.add(dir)
         return json_dirs
 
+
+    def setup_logger(self):
+
+        """
+        Setup logger
+        """
+
+        # TBD: This could be configurable instead of hard-coded.
+        home_dir = str(Path.home())
+        log_dir = os.path.join(home_dir, "archive_crawler_logs")
+
+        # If the directory doesn't exist, make it
+        if not os.path.isdir(log_dir):
+            try:
+                Path(log_dir).mkdir(parents = True, exist_ok = True)
+            except Exception as e:
+                sys.exit(f"Attempt to make logging directory {log_dir} failed, received exception {str(e)}")
+
+
+        # Create the log file, with a timestamp in its name, and get its full path
+        log_file = "archive_crawler_log_" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + ".txt"
+        log_path = os.path.join(log_dir, log_file)
+        print(f"Log file located at {log_path}")
+
+        # TBD: The desired log level should also be configurable
+        log_level = "DEBUG"
+
+        # Now configure the logger with the established parameters.
+        logging.basicConfig(filename=log_path, filemode='w', level=log_level,
+            format='%(asctime)s %(levelname)s: %(message)s')
 
 
 if __name__ == "__main__":
